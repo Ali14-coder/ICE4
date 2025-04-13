ce4ebb9433ff99b1b8332bfd6d41f5c61b864ded
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-13 22:29:53 +0200

    Updated CRUD MVC components

--- PROG_MVC_ICE4/Controllers/CustomerController.cs
+++ PROG_MVC_ICE4/Controllers/CustomerController.cs
@@ -8,5 +8,15 @@ namespace PROG_MVC_ICE4.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string? test = null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
