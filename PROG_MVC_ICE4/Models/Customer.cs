483753dc6e6afcbc537c1d6ab43d74762bebb944
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-13 20:34:27 +0200

    Home MVC UI created

--- PROG_MVC_ICE4/Models/Customer.cs
+++ PROG_MVC_ICE4/Models/Customer.cs
@@ -1,6 +1,27 @@
-﻿namespace PROG_MVC_ICE4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG_MVC_ICE4.Models
 {
     public class Customer
     {
+        [Key]
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Surname { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Password { get; set; }
     }
 }
--- PROG_MVC_ICE4/Models/Product.cs
+++ PROG_MVC_ICE4/Models/Product.cs
@@ -1,6 +1,29 @@
-﻿namespace PROG_MVC_ICE4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG_MVC_ICE4.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public String ProductName { get; set; }
+
+        [Required]
+
+        public double Price { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
     }
 }
